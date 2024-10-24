a840c8e7fa9cf499279a65894b8f8c2048364966
shubho365 <shubhomoy365@gmail.com>
2024-10-24 10:14:26 +0530

    Sqaure and Cube implemented

--- calculator.c
+++ calculator.c
@@ -20,6 +20,14 @@ float divide(int i, int j) {
     return ((float) i / j);  // Explicit type casting to float for decimal results.
 }
 
+int square(int i) {
+	return i * i;
+}
+
+int cube(int i) {
+	return i * i * i;
+}
+
 int main() {
     int a, b;
     char operation;
@@ -64,12 +72,12 @@ int main() {
             case '^':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The result of %d^2 is %d.\n", a, square(a));
                 break;
             case '#':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The result of %d^3 is %d.\n", a, cube(a));
                 break;
             case 'i':
                 printf("Enter an integer: ");
