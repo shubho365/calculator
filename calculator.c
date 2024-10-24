02136bcf244cbce5f28f6ce231409c575ef49f6e
shubho365 <shubhomoy365@gmail.com>
2024-10-24 10:44:27 +0530

    Inverse implemented

--- calculator.c
+++ calculator.c
@@ -28,6 +28,14 @@ int cube(int i) {
 	return i * i * i;
 }
 
+float inverse(int i) {
+	if (i == 0) {
+		printf("Error: Inverse of zero is not allowed.\n");
+		return 0; // Return 0 to indicate an error.
+	}
+	return 1.0 / i;
+}
+
 int main() {
     int a, b;
     char operation;
@@ -79,10 +87,10 @@ int main() {
                 scanf("%d", &a);
                 printf("The result of %d^3 is %d.\n", a, cube(a));
                 break;
-            case 'i':
+	    case 'i':
                 printf("Enter an integer: ");
                 scanf("%d", &a);
-                printf("Will be implemented soon...\n");
+                printf("The result of %d inverse is %.2f.\n", a, inverse(a));
                 break;
             default:
                 printf("Invalid operation. Please try again.\n");
